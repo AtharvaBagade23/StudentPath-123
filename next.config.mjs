1f0ab384db8e2d1be26d7859a0297039cefe1c8b
Yash Chavan <yashvchavan07@gmail.com>
2025-09-14 16:18:47 +0530

    deploy commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // Uncomment the following line if you want to disable ESLint during production builds
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
+    domains: ['localhost'],
     unoptimized: true,
   },
+  // Optimize for production
+  compress: true,
+  productionBrowserSourceMaps: false,
+  optimizeFonts: true,
 }
 
 export default nextConfig
